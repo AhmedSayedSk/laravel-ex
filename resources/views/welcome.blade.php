e319726b17ce1e106675214bb512358c06d30b80
Ahmed Sayed Sk <AhmedSayedSk@users.noreply.github.com>
2017-05-15 15:01:50 +0200

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">test</div>
+                <div class="title">test2</div>
             </div>
         </div>
     </body>
