4bb3acf5ac452a4c018ab061559fc0127d354599
Ahmedsk <ahmedsk128@gmail.com>
2017-07-07 19:48:32 +0200

    edit3

--- migrate-database.sh
+++ migrate-database.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e 
 
+echo "Update .env 'php artisan migrate --env=production"
+php artisan migrate --env=production
+
 echo "Migrating database 'php artisan migrate --force'..."
 php artisan migrate --force
 
