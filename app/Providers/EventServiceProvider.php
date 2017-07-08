f35d423aa9e122017f9bbdbc84bc39b2d84826ff
Ahmedsk <ahmedsk128@gmail.com>
2017-07-08 05:00:47 +0200

    edit6

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -5,16 +5,29 @@ namespace App\Providers;
 use Illuminate\Contracts\Events\Dispatcher as DispatcherContract;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
-class EventServiceProvider extends ServiceProvider {
-
+class EventServiceProvider extends ServiceProvider
+{
+    /**
+     * The event listener mappings for the application.
+     *
+     * @var array
+     */
     protected $listen = [
-        'App\Events\ProductsViewCounterEvent' => [
-            'App\Listeners\ProductsViewCounterListener',
+        'App\Events\SomeEvent' => [
+            'App\Listeners\EventListener',
         ],
     ];
 
-    public function boot(DispatcherContract $events) {
+    /**
+     * Register any other events for your application.
+     *
+     * @param  \Illuminate\Contracts\Events\Dispatcher  $events
+     * @return void
+     */
+    public function boot(DispatcherContract $events)
+    {
         parent::boot($events);
-        $events->listen('products.view_counter', 'App\Events\ProductsViewCounterEvent');
+
+        //
     }
 }
